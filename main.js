a803eed7b2e1a085b2a94bb922c733bb43634af3
Bbbbbella <56407654+Bbbbbella@users.noreply.github.com>
2019-11-03 21:37:00 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+﻿
+
+$(function () {
+    
+    $("#addRec").click(function () {
+        var startYearDoc = $("#startYear");
+        var startYear = startYearDoc.val();
+        if (!Number(startYear) || Number(startYear) < 2000) {
+            alert("Incorrect year:" + startYear);
+            return false;
+        }
+        var fullName = $("#fullName").val();
+        var Major = $("#Major").val();
+        var myDate = new Date();
+        var nowDate = myDate.getHours() + ":" + myDate.getMinutes();
+        var printStr = nowDate + " - " + fullName + ", " + Major + ", " + startYear;
+        $("#records").append("<p class='title'>" + printStr+"</p>");
+    });
+});
